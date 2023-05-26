0797f9fc364633e3b9c96fb505ff3b3a3c3af909
Lilly <71640994+Lilly05@users.noreply.github.com>
2023-05-26 10:47:53 +0200

    Add files via upload

---
+++ PowerStatistic/database.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+# Linux: export CONNECTION_STRING=""
+# printenv CONNECTION_STRING
+
+# Windows: set CONNECTION_STRING="" | $CONNECTION_STRING=""
+# echo CONNECTION_STRING
+
+class Database:
+    def __init__(self):
+        return    
+    
+    def getDatabase():
+        db = MongoClient['Aufgabe4']
+
+    def getCollection();
+        collection = db['PowerStatistics']
